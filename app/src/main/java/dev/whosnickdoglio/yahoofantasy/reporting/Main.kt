539ede47ccf2e2b040696d67593d9e1e4f9fddee
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-10-15 00:04:07 -0400

    Add other league info

--- app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/Main.kt
+++ app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/Main.kt
@@ -3,8 +3,10 @@ package dev.whosnickdoglio.yahoofantasy.reporting
 import dev.whosnickdoglio.yahoofantasy.reporting.data.LeagueInfo
 import dev.whosnickdoglio.yahoofantasy.reporting.di.AppDependencyGraph
 import dev.zacsweers.metro.createGraphFactory
-import kotlinx.datetime.LocalDate
+import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.DateTimePeriod
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
@@ -12,11 +14,21 @@ import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
 public suspend fun main(args: Array<String>) {
-    val leagueInfo = LeagueInfo.MitchRobLeagueInfo
-    // 2025-10-24
-    // 2025-10-21
-//    val yesterday = Clock.System.now().toLocalDateTime(TimeZone.UTC).date
-    val yesterday = LocalDate(year = 2025, month = 10, day = 24)
-    val graph = createGraphFactory<AppDependencyGraph.Factory>().create(yesterday, leagueInfo)
+    val leagueInfo = when (val league = args.first()) {
+        "mitch" -> LeagueInfo.MitchRobLeagueInfo
+        "redacted" -> LeagueInfo.Redacted
+        "birthday" -> LeagueInfo.BirthdayCakeOreo
+        else -> error("Unknown league $league")
+    }
+
+    val yesterday = Clock.System.now().toLocalDateTime(TimeZone.UTC).date.minus(DatePeriod(
+        years = 0,
+        months = 0,
+        days = 1
+    ))
+    val graph = createGraphFactory<AppDependencyGraph.Factory>().create(
+        yesterday = yesterday,
+        leagueInfo = leagueInfo
+    )
     graph.app.main()
 }
--- app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/data/LeagueInfo.kt
+++ app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/data/LeagueInfo.kt
@@ -10,11 +10,33 @@ internal interface LeagueInfo {
 
     data object MitchRobLeagueInfo : LeagueInfo {
         override val name: String = "Mitch Rob"
-        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/197286"
-        override val numberOfTeams: Int = 10
+        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/9615"
+        override val numberOfTeams: Int = 12
         override val spreadSheetInfo: SpreadSheetInfo = SpreadSheetInfo(
             id = "11LQZQF2CDX4lmChkryV00aD6I2XTi3Fp4BmqNVwtuEI",
             range = "MitchRob!A1:E1"
+        )
+        override val injuryListCount: Int = 4
+    }
+
+    data object Redacted: LeagueInfo {
+        override val name: String = "Redacted"
+        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/69734"
+        override val numberOfTeams: Int = 12
+        override val spreadSheetInfo: SpreadSheetInfo = SpreadSheetInfo(
+            id = "11LQZQF2CDX4lmChkryV00aD6I2XTi3Fp4BmqNVwtuEI",
+            range = "Redacted!A1:E1"
+        )
+        override val injuryListCount: Int = 4
+    }
+
+    data object BirthdayCakeOreo: LeagueInfo {
+        override val name: String = "Birthday Cake"
+        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/9616"
+        override val numberOfTeams: Int = 10
+        override val spreadSheetInfo: SpreadSheetInfo = SpreadSheetInfo(
+            id = "11LQZQF2CDX4lmChkryV00aD6I2XTi3Fp4BmqNVwtuEI",
+            range = "Birthday!A1:E1"
         )
         override val injuryListCount: Int = 4
     }
