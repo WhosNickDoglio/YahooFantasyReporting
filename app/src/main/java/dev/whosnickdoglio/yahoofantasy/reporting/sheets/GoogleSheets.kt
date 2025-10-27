78eab1c118df23195cf4a221b7bc567404488509
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-10-27 18:20:30 -0400

    Make more things internal now that Mockingbird supports it

--- app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/sheets/GoogleSheets.kt
+++ app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/sheets/GoogleSheets.kt
@@ -4,11 +4,11 @@ package dev.whosnickdoglio.yahoofantasy.reporting.sheets
 
 import java.time.LocalDate
 
-public interface GoogleSheets {
-    public suspend fun sendReport(teamReport: List<GoogleSheetsTeamReport>)
+internal interface GoogleSheets {
+    suspend fun sendReport(teamReport: List<GoogleSheetsTeamReport>)
 }
 
-public data class GoogleSheetsTeamReport(
+internal data class GoogleSheetsTeamReport(
     val date: LocalDate,
     val teamName: String,
     val healthyOnInjuryList: Boolean,
