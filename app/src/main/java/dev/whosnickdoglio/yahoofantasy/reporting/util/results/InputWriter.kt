6118835cf69207a4011afa8b55ff32b90fdd5039
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-11-04 17:26:32 -0500

    Revert "Include date in JSON file name"
    This reverts commit 7fc50194c5a16851294c32d27f2e35103f479755.

--- app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/util/results/InputWriter.kt
+++ app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/util/results/InputWriter.kt
@@ -8,7 +8,6 @@ import dev.whosnickdoglio.yahoofantasy.reporting.util.log.SimpleLogger
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import java.io.File
-import java.time.LocalDate
 import kotlinx.serialization.json.Json
 
 internal fun interface InputWriter {
@@ -19,11 +18,10 @@ internal fun interface InputWriter {
 internal class DefaultInputWriter(
     private val logger: SimpleLogger,
     private val leagueInfo: LeagueInfo,
-    private val date: LocalDate,
 ) : InputWriter {
     override suspend fun write(input: List<RosterInfo>) {
         val jobId = System.getenv("GITHUB_RUN_ID")
-        val rawDataFile = File("${leagueInfo.name}_${jobId}_$date.json")
+        val rawDataFile = File("${leagueInfo.name}_raw_input_$jobId.json")
         rawDataFile.writeText(Json.encodeToString(input))
         logger.log("Successfully wrote raw input to ${rawDataFile.absolutePath}")
     }
