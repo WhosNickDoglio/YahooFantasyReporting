10254f69d48049700509f1aec30a416f1619c3fd
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-11-09 10:29:33 -0500

    WIP migrating to use Clikt for easier argument passing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ tasks.withType(Detekt::class).configureEach {
 }
 
 dependencies {
+    implementation(libs.clikt)
     implementation(libs.google.api.client)
     implementation(libs.google.sheets)
     implementation(libs.kotlinx.coroutines.core)
@@ -32,6 +33,7 @@ dependencies {
     implementation(libs.ksoup)
     implementation(libs.ksoup.network)
 
+    // https://www.slf4j.org/codes.html#noProviders
     runtimeOnly(libs.slf4j)
 
     testImplementation(libs.assertk)
