bfde1744306f54bb1b595e4f66645499aad0372b
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-11-09 10:44:28 -0500

    Re-enable Google sheets upload

--- app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/sheets/DefaultGoogleSheetsClient.kt
+++ app/src/main/java/dev/whosnickdoglio/yahoofantasy/reporting/sheets/DefaultGoogleSheetsClient.kt
@@ -1,15 +1,15 @@
 // Copyright (C) 2025 Nicholas Doglio
 // SPDX-License-Identifier: MIT
-@file:Suppress("UnusedPrivateProperty")
-
 package dev.whosnickdoglio.yahoofantasy.reporting.sheets
 
 import com.google.api.services.sheets.v4.Sheets
+import com.google.api.services.sheets.v4.model.ValueRange
 import dev.whosnickdoglio.yahoofantasy.reporting.data.LeagueInfo
 import dev.whosnickdoglio.yahoofantasy.reporting.util.coroutines.IoDispatcher
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.withContext
 
 @ContributesBinding(AppScope::class)
 internal class DefaultGoogleSheetsClient(
@@ -17,19 +17,19 @@ internal class DefaultGoogleSheetsClient(
     private val leagueInfo: LeagueInfo,
     @param:IoDispatcher private val ioCoroutineContext: CoroutineContext,
 ) : GoogleSheets {
-    override suspend fun sendReport(teamReport: List<GoogleSheetsTeamReport>): Unit = Unit
-    //        withContext(ioCoroutineContext) {
-    //            require(teamReport.isNotEmpty()) { "Team report cannot be empty" }
-    //            with(sheets.spreadsheets().values()) {
-    //                    append(
-    //                            SPREADSHEET_ID,
-    //                            "${leagueInfo.spreadSheetName}$SPREADSHEET_INPUT_RANGE",
-    //                            ValueRange().setValues(teamReport.map { it.toList() }),
-    //                        )
-    //                        .setValueInputOption("USER_ENTERED")
-    //                }
-    //                .execute()
-    //        }
+    override suspend fun sendReport(teamReport: List<GoogleSheetsTeamReport>): Unit =
+        withContext(ioCoroutineContext) {
+            require(teamReport.isNotEmpty()) { "Team report cannot be empty" }
+            with(sheets.spreadsheets().values()) {
+                    append(
+                            SPREADSHEET_ID,
+                            "${leagueInfo.spreadSheetName}$SPREADSHEET_INPUT_RANGE",
+                            ValueRange().setValues(teamReport.map { it.toList() }),
+                        )
+                        .setValueInputOption("USER_ENTERED")
+                }
+                .execute()
+        }
 }
 
 private const val SPREADSHEET_ID = "11LQZQF2CDX4lmChkryV00aD6I2XTi3Fp4BmqNVwtuEI"
