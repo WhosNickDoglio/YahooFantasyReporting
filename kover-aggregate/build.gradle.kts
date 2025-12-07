0ef86ffaccd45a9381281f78347db562e5d61895
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-12-07 11:06:45 -0500

    Include new modules in code coverage reporting

--- kover-aggregate/build.gradle.kts
+++ kover-aggregate/build.gradle.kts
@@ -8,4 +8,8 @@ kover { reports { filters { excludes { classes("*\$Metro*") } } } }
 /*
 This module just exists so Kover can create a merged report for code coverage.
  */
-dependencies { kover(projects.app) }
+dependencies {
+    kover(projects.app)
+    kover(projects.simpleLogger)
+    kover(projects.leagueInfo)
+}
