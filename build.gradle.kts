adb5311eb043bf35e003d647893417c1da10de8e
nicholasdoglio <NicholasDoglio@Gmail.com>
2026-02-28 15:18:03 -0500

    Fix ordering of root plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ buildscript { dependencies { classpath(libs.burst) } }
 plugins {
     alias(libs.plugins.dependencyAnalysis)
     alias(libs.plugins.doctor)
+    alias(libs.plugins.kover)
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kover)
     alias(libs.plugins.ktfmt) apply false
     alias(libs.plugins.metro) apply false
     alias(libs.plugins.convention.jvm) apply false
