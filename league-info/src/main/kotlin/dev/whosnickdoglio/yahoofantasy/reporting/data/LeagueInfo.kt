51b446b7789ebe000a23df59735971416f118145
nicholasdoglio <NicholasDoglio@Gmail.com>
2026-09-05 15:45:26 -0400

    Add Sport enum to add football support

--- league-info/src/main/kotlin/dev/whosnickdoglio/yahoofantasy/reporting/data/LeagueInfo.kt
+++ league-info/src/main/kotlin/dev/whosnickdoglio/yahoofantasy/reporting/data/LeagueInfo.kt
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Nicholas Doglio
+// Copyright (C) 2026 Nicholas Doglio
 // SPDX-License-Identifier: MIT
 
 package dev.whosnickdoglio.yahoofantasy.reporting.data
@@ -6,6 +6,7 @@ package dev.whosnickdoglio.yahoofantasy.reporting.data
 // TODO dynamically get game key
 public interface LeagueInfo {
     public val name: String
+    public val sport: Sport
     public val baseUrl: String
     public val numberOfTeams: Int
     public val injuryListCount: Int
@@ -13,7 +14,8 @@ public interface LeagueInfo {
 
     public data object MitchRobLeagueInfo : LeagueInfo {
         override val name: String = "mitch"
-        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/9615"
+        override val sport: Sport = Sport.Basketball
+        override val baseUrl: String = "${sport.url}/9615"
         override val numberOfTeams: Int = 12
         override val spreadSheetName: String = "MitchRob"
         override val injuryListCount: Int = 4
@@ -21,7 +23,8 @@ public interface LeagueInfo {
 
     public data object Redacted : LeagueInfo {
         override val name: String = "redacted"
-        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/69734"
+        override val sport: Sport = Sport.Basketball
+        override val baseUrl: String = "${sport.url}/69734"
         override val numberOfTeams: Int = 12
         override val spreadSheetName: String = "Redacted"
         override val injuryListCount: Int = 4
@@ -29,9 +32,15 @@ public interface LeagueInfo {
 
     public data object BirthdayCakeOreo : LeagueInfo {
         override val name: String = "birthday"
-        override val baseUrl: String = "https://basketball.fantasysports.yahoo.com/nba/9616"
+        override val sport: Sport = Sport.Basketball
+        override val baseUrl: String = "${sport.url}/9616"
         override val numberOfTeams: Int = 10
         override val spreadSheetName: String = "Birthday"
         override val injuryListCount: Int = 4
     }
+}
+
+public enum class Sport(public val url: String) {
+    Basketball("https://basketball.fantasysports.yahoo.com/nba"),
+    Football("https://football.fantasysports.yahoo.com/f1/"),
 }
--- league-info/src/testFixtures/kotlin/dev/whosnickdoglio/yahoofantasy/reporting/data/FakeLeagueInfo.kt
+++ league-info/src/testFixtures/kotlin/dev/whosnickdoglio/yahoofantasy/reporting/data/FakeLeagueInfo.kt
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Nicholas Doglio
+// Copyright (C) 2026 Nicholas Doglio
 // SPDX-License-Identifier: MIT
 
 package dev.whosnickdoglio.yahoofantasy.reporting.data
@@ -6,6 +6,7 @@ package dev.whosnickdoglio.yahoofantasy.reporting.data
 data class FakeLeagueInfo(
     override val name: String = "Mitch Rob",
     override val baseUrl: String = "example.com",
+    override val sport: Sport = Sport.Basketball,
     override val numberOfTeams: Int = 12,
     override val injuryListCount: Int = 4,
     override val spreadSheetName: String = "MitchRob",
